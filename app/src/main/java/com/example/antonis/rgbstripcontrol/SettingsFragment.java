35e09200d3759b0229f93bef80f1c7ac21399f56
Antonis Klironomos <antonisklironomos@gmail.com>
2018-11-12 23:53:53 +0200

    Added save functionality for settings

--- app/src/main/java/com/example/antonis/rgbstripcontrol/SettingsFragment.java
+++ app/src/main/java/com/example/antonis/rgbstripcontrol/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.antonis.rgbstripcontrol;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -13,55 +15,65 @@ import android.widget.EditText;
 
 public class SettingsFragment extends DialogFragment {
 
-	String ipAddressStr;
-	EditText ipAddress;
-	Button submitSettingsButton;
+    String ipAddressStr;
+    EditText ipAddress;
+    Button submitSettingsButton;
+    SharedPreferences storedSettings;
 
-	private OnSubmitSettingsListener onSubmitSettingsListener;
+    private OnSubmitSettingsListener onSubmitSettingsListener;
 
-	public static interface OnSubmitSettingsListener {
-		void OnSubmitSettings(String ipAddress);
-	}
+    public static interface OnSubmitSettingsListener {
+        void OnSubmitSettings(String ipAddress);
+    }
 
-	public void setDefaultIpAddress(String ipAddress) {
-		ipAddressStr = ipAddress;
-	}
+    public void setDefaultIpAddress(String ipAddress) {
+        ipAddressStr = ipAddress;
+    }
 
-	public void setOnSubmitSettingsListener(OnSubmitSettingsListener onSubmitSettingsListener) {
-		this.onSubmitSettingsListener = onSubmitSettingsListener;
-	}
+    public void setOnSubmitSettingsListener(OnSubmitSettingsListener onSubmitSettingsListener) {
+        this.onSubmitSettingsListener = onSubmitSettingsListener;
+    }
 
-	public SettingsFragment() {
-		// Required empty public constructor
-	}
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
 
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View fragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
 
-		ipAddress = fragmentView.findViewById(R.id.text_ip_address);
-		submitSettingsButton = fragmentView.findViewById(R.id.button_submit_settings);
+        storedSettings = getActivity().getPreferences(Context.MODE_PRIVATE);
+        ipAddress = fragmentView.findViewById(R.id.text_ip_address);
+        submitSettingsButton = fragmentView.findViewById(R.id.button_submit_settings);
 
-		getDialog().setTitle("Settings");
-		if (ipAddressStr != null)
-			ipAddress.setText(ipAddressStr);
+        getDialog().setTitle("Settings");
 
-		submitSettingsButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (!ipAddress.getText().toString().equals(""))
-					onSubmitSettingsListener.OnSubmitSettings(ipAddress.getText().toString());
-				dismiss();
-			}
-		});
 
-		return fragmentView;
-	}
+        String storedIpAddress = storedSettings.getString("IP", ipAddressStr);
 
-	@Override
-	public void onResume() {
-		super.onResume();
-	}
+        if (storedIpAddress != null)
+            ipAddress.setText(storedIpAddress);
+
+        submitSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!ipAddress.getText().toString().equals("")) {
+                    onSubmitSettingsListener.OnSubmitSettings(ipAddress.getText().toString());
+                    SharedPreferences.Editor editor = storedSettings.edit();
+                    editor.putString("IP", ipAddress.getText().toString());
+                    editor.apply();
+                }
+                dismiss();
+            }
+        });
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
